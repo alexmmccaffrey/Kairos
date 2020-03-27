fe39483066c2b5052f3b47f8f05aa71e9fefea64
Alex McCaffrey <alexmccaffrey@macbook-pro.home>
2020-03-26 21:38:10 -0700

    Test 2

---
+++ Kairos/Kairos/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Kairos
+//
+//  Created by Alex McCaffrey on 3/26/20.
+//  Copyright © 2020 Alex McCaffrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
