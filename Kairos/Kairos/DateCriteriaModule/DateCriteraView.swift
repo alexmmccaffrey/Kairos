51511b32b9d29f80078654e5749c8b75a3b57df6
Alex McCaffrey <alexmccaffrey@alexs-mbp.home>
2020-09-10 06:00:37 -0700

    test

---
+++ Kairos/Kairos/DateCriteriaModule/DateCriteraView.swift
@@ -0,0 +1,21 @@
+//
+//  DateCriteraView.swift
+//  Kairos
+//
+//  Created by Alex McCaffrey on 8/25/20.
+//  Copyright © 2020 Alex McCaffrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct DateCriteraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateCriteraView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateCriteraView()
+    }
+}
---
+++ Kairos/Kairos/DateCriteriaModule/DateCriteriaInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Kairos
+//
+//  Created by Alex McCaffrey on 8/25/20.
+//  Copyright © 2020 Alex McCaffrey. All rights reserved.
+//
+
+import Foundation
